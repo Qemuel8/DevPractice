77edd0ce8d185d677cb089cf5c92e0db610a87d7
Qemuel <yigitalsancak8@gmail.com>
2023-05-11 11:41:11 +0300

    adding practice files

---
+++ React Course/learnreact/src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import img from "./react.png";
+
+function List() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src={img} width="40px" />
+        </nav>
+      </header>
+      <h1> Reasons im excited to learn React</h1>
+      <ul>
+        <li> React is exciting because it feels like playing with lego</li>
+        <li>gave me a job and i enjoy working on crafting things with it</li>
+        <li>its a fun syntax</li>
+      </ul>
+      <footer>
+        <small>© 2023 qemuel development. All rights reserved</small>
+      </footer>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<List />);
